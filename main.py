7823a135d3882f04af229c4f0a6fe28271f871f3
Mark Peters <mapeters101@earthlink.net>
2024-09-17 13:51:48 -0400

    Send email

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 import selectorlib
+import smtplib, ssl
+import os
 
 URL = "http://programmer100.pythonanywhere.com/tours"
 HEADERS = {
@@ -19,7 +21,22 @@ def extract(source):
     return value
 
 
-def send_email():
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "myswapps101@gmail.com"
+    password = os.getenv("PASSWORD_MYSWAPPS101")
+
+    receiver = "myswapps101@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
+
+
     print("Email was sent!")
 
 
@@ -40,6 +57,6 @@ if __name__ == "__main__":
     if extracted != "No upcoming tours":
         if extracted not in content:
             store(extracted)
-            send_email()
+            send_email(message="Hey, new event was found!")
 
 
